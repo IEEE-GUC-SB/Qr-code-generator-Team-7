cb3640fabf7d56555948ffb4fdeeb963eb96bed6
Omar Shaaban <98961039+Omar-Fahim@users.noreply.github.com>
2022-11-10 20:07:00 +0200

    Read-Write
    A code allowing us to read the required data from the excel sheet and inserting the result back in the excel sheet after doing the computation

--- qr generator.py
+++ qr generator.py
@@ -1,5 +1,9 @@
 import pyimgur
 import qrcode
+import xlrd
+from xlutils.copy import copy
+
+
 CLIENT_ID = '7aa8b838eb5897a'
 im = pyimgur.Imgur(CLIENT_ID)
 
@@ -7,16 +11,35 @@ im = pyimgur.Imgur(CLIENT_ID)
 def gen(data, id):
     qr = qrcode.QRCode(
         version=3, error_correction=qrcode.constants.ERROR_CORRECT_L)
-    data2 = data + "_" + str(id)
+    data2 = data + "\n" + "IEEE-"+str(id)
     qr = qrcode.make(data2)
     temp = str(id) + ".png"
     qr.save(temp)
     PATH = temp
     uploaded_img = im.upload_image(PATH, title="NODE")
-    print("uploaded Link: ", uploaded_img.link)
+    Link = uploaded_img.link
+    write(Link,id)
 
 
-for i in range(2):
-    # here we read the names and emails
-    gen("Name_Email", i)
-    #  the i will be the id
+
+def write(link,row):
+    rb = xlrd.open_workbook('Book1.xls')
+    wb = copy(rb)
+    w_sheet = wb.get_sheet(0)
+    w_sheet.write(row,2,link)
+    wb.save('Book1.xls')
+    
+
+
+
+workbook = xlrd.open_workbook(r"Book1.xls")
+sheet = workbook.sheet_by_index(0)
+row_count = sheet.nrows
+for cur_row in range(0, row_count):
+    cell_name = sheet.cell(cur_row, 0)
+    cell_email = sheet.cell(cur_row, 1)
+    name = cell_name.value
+    email=cell_email.value
+    gen(name+"\n"+email,cur_row)
+
+
